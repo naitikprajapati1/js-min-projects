e9fc57c1f4b184d4b9a96135ebd91b921f0131c0
naitikprajapati1 <naitikp619@gmail.com>
2023-05-31 19:54:50 +0530

    Add tutortify html css lading page

---
+++ Tutorify/index.js
@@ -0,0 +1,17 @@
+AOS.init({
+    duration: 1200,
+  })
+
+  // nav code
+  // create a hamburer menu with
+const toggleMenu =()=>{
+  console.log('clicking toggleMenu');
+  document.body.classList.toggle('active-menu');
+}
+
+  // let menu=document.querySelector('.menu-box');
+  // let hamburgur=document.querySelector('.ham-burgur');
+
+  // hamburgur.addEventListener('click',()=>{
+  //   document.body.classList.toggle('active-menu');
+  // })
